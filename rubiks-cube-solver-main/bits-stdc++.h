0943b3e71ccba86c7e4eb92c322cc36ba5b88c7d
ThedRk1607 <128738915+ThedRk1607@users.noreply.github.com>
2024-09-01 01:00:20 +0530

    Add files via upload
    RubiksCubeSolver Project 
    1. Modeled a virtual Rubik's Cube(3x3) in three different Models/Classes using different data-structures in
    c++.
    2. Achieve a solving time under 3 seconds for a Rubik's Cube jumbled 8 times using BFS, DFS and IDDFS
    Algorithms.
    3. Implemented the Korf's IDA* Algorithm, achieving a solving time under 10 seconds for a Rubik's Cube
    jumbled 13 times.

---
+++ rubiks-cube-solver-main/bits-stdc++.h
@@ -0,0 +1,110 @@
+//
+// Created by deepak kumar on 26/08/24.
+//
+
+#ifndef RUBIKS_CUBE_SOLVER_MAIN_BITS_STDC_H
+#define RUBIKS_CUBE_SOLVER_MAIN_BITS_STDC_H
+
+#endif //RUBIKS_CUBE_SOLVER_MAIN_BITS_STDC_H
+// bits/stdc++.h
+// Inclusion of all standard libraries
+
+#ifndef _GLIBCXX_NO_ASSERT
+#include <cassert>
+#endif
+
+#include <cctype>
+#include <cerrno>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+#include <cfloat>
+#include <ciso646>
+#include <cstdint>
+#include <cwchar>
+#include <cwctype>
+
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#include <algorithm>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <codecvt>
+#include <condition_variable>
+#include <forward_list>
+#include <initializer_list>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <typeinfo>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <valarray>
+#include <vector>
